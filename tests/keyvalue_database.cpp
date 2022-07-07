8820d01f86da8cf119e7540e64c23f3937995ff6
Fedor Semenov <fedor.v.semenov@gmail.com>
2022-07-07 09:26:23 +0500

    fix: Fixed `keyvalue_database` test.

--- tests/keyvalue_database.cpp
+++ tests/keyvalue_database.cpp
@@ -24,52 +24,62 @@ void check_set_get (pfs::filesystem::path const & db_path)
 {
     using database_t = debby::keyvalue_database<Backend>;
 
-    if (fs::exists(db_path) && fs::is_regular_file(db_path))
-        fs::remove(db_path);
+    if (fs::exists(db_path))
+        fs::remove_all(db_path);
 
-    auto db = database_t::make(db_path);
+    TRY {
+        auto db = database_t::make(db_path);
 
-    REQUIRE(db);
+        REQUIRE(db);
 
-    db.set("bool"  , true);
-    db.set("int8"  , std::numeric_limits<std::int8_t>::min());
-    db.set("uint8" , std::numeric_limits<std::uint8_t>::max());
-    db.set("int16" , std::numeric_limits<std::int16_t>::min());
-    db.set("uint16", std::numeric_limits<std::uint16_t>::max());
-    db.set("int32" , std::numeric_limits<std::int32_t>::min());
-    db.set("uint32", std::numeric_limits<std::uint32_t>::max());
-    db.set("int64" , std::numeric_limits<std::int64_t>::min());
-    db.set("uint64", std::numeric_limits<std::uint64_t>::max());
-    db.set("float" , static_cast<float>(3.14159));
-    db.set("double", static_cast<double>(3.14159));
-    db.set("text"  , std::string{"Hello"});
-    db.set("empty" , std::string{""});
-    db.set("cstr"  , "World");
+        db.set("bool"  , true);
+        db.set("int8"  , std::numeric_limits<std::int8_t>::min());
+        db.set("uint8" , std::numeric_limits<std::uint8_t>::max());
+        db.set("int16" , std::numeric_limits<std::int16_t>::min());
+        db.set("uint16", std::numeric_limits<std::uint16_t>::max());
+        db.set("int32" , std::numeric_limits<std::int32_t>::min());
+        db.set("uint32", std::numeric_limits<std::uint32_t>::max());
+        db.set("int64" , std::numeric_limits<std::int64_t>::min());
+        db.set("uint64", std::numeric_limits<std::uint64_t>::max());
+        db.set("float" , static_cast<float>(3.14159));
+        db.set("double", static_cast<double>(3.14159));
+        db.set("text"  , std::string{"Hello"});
+        db.set("empty" , std::string{""});
+        db.set("cstr"  , "World");
 
-    REQUIRE_EQ(db.template get_or<int>("unknown", -1), -1);
+        REQUIRE_EQ(db.template get_or<int>("unknown", -1), -1);
 
-    REQUIRE_EQ(db.template get<bool>("bool")            , true);
-    REQUIRE_EQ(db.template get<std::int8_t>("int8")     , std::numeric_limits<std::int8_t>::min());
-    REQUIRE_EQ(db.template get<std::uint8_t>("uint8")   , std::numeric_limits<std::uint8_t>::max());
-    REQUIRE_EQ(db.template get<std::int16_t>("int16")   , std::numeric_limits<std::int16_t>::min());
-    REQUIRE_EQ(db.template get<std::uint16_t>("uint16") , std::numeric_limits<std::uint16_t>::max());
-    REQUIRE_EQ(db.template get<std::int32_t>("int32")   , std::numeric_limits<std::int32_t>::min());
-    REQUIRE_EQ(db.template get<std::uint32_t>("uint32") , std::numeric_limits<std::uint32_t>::max());
-    REQUIRE_EQ(db.template get<std::int64_t>("int64")   , std::numeric_limits<std::int64_t>::min());
-    REQUIRE_EQ(db.template get<std::uint64_t>("uint64") , std::numeric_limits<std::uint64_t>::max());
-    REQUIRE(std::abs(db.template get<float>("float") - static_cast<float>(3.14159)) < float{0.001});
-    REQUIRE(std::abs(db.template get<double>("double") - static_cast<double>(3.14159)) < double(0.001));
+        REQUIRE_EQ(db.template get<bool>("bool")            , true);
+        REQUIRE_EQ(db.template get<std::int8_t>("int8")     , std::numeric_limits<std::int8_t>::min());
+        REQUIRE_EQ(db.template get<std::uint8_t>("uint8")   , std::numeric_limits<std::uint8_t>::max());
+        REQUIRE_EQ(db.template get<std::int16_t>("int16")   , std::numeric_limits<std::int16_t>::min());
+        REQUIRE_EQ(db.template get<std::uint16_t>("uint16") , std::numeric_limits<std::uint16_t>::max());
+        REQUIRE_EQ(db.template get<std::int32_t>("int32")   , std::numeric_limits<std::int32_t>::min());
+        REQUIRE_EQ(db.template get<std::uint32_t>("uint32") , std::numeric_limits<std::uint32_t>::max());
+        REQUIRE_EQ(db.template get<std::int64_t>("int64")   , std::numeric_limits<std::int64_t>::min());
+        REQUIRE_EQ(db.template get<std::uint64_t>("uint64") , std::numeric_limits<std::uint64_t>::max());
+        REQUIRE(std::abs(db.template get<float>("float") - static_cast<float>(3.14159)) < float{0.001});
+        REQUIRE(std::abs(db.template get<double>("double") - static_cast<double>(3.14159)) < double(0.001));
 
-    REQUIRE_EQ(db.template get<std::string>("text"), std::string{"Hello"});
-    REQUIRE(db.template get<std::string>("empty").empty());
+        REQUIRE_EQ(db.template get<std::string>("text"), std::string{"Hello"});
+        REQUIRE(db.template get<std::string>("empty").empty());
 
-    db.remove("text");
-    REQUIRE(db.template get_or<std::string>("text", "").empty());
+        db.remove("text");
+        REQUIRE(db.template get_or<std::string>("text", "").empty());
 
-    // FIXME Cause abnormal finishing
-    //db.destroy();
-    //REQUIRE(!db);
-    fs::remove_all(db_path);
+        // FIXME Cause abnormal finishing
+        //db.destroy();
+        //REQUIRE(!db);
+    } CATCH(debby::error ex) {
+#if PFS__EXCEPTIONS_ENABLED
+        REQUIRE_MESSAGE(false, ex.what());
+#endif
+    }
+
+    // In Windows database must be closed/destructed before to avoid exception:
+    // "The process cannot access the file because it is being used by another process"
+    if (fs::exists(db_path))
+        fs::remove_all(db_path);
 }
 
 #if DEBBY__ROCKSDB_ENABLED
