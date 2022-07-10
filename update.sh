6e8570e8c05e0a61c410eb5bce403c6a1ed09469
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 18:33:12 +0500

    Updated `update.sh`.

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ ROCKSDB_RELEASE=v6.29.5
 
 if [ -e .git ] ; then
 
-    git pull \
+    git checkout master && git pull origin master \
         && git submodule update --init \
         && git submodule update --remote \
-        && cd 3rdparty/portable-target && git checkout master && git pull \
+        && cd 3rdparty/portable-target && git checkout master && git pull origin master \
         && cd $CWD \
-        && cd 3rdparty/pfs/common && git checkout master && git pull  \
+        && cd 3rdparty/pfs/common && ./update.sh \
         && cd $CWD \
         && cd 3rdparty/rocksdb && git checkout $ROCKSDB_RELEASE
 
