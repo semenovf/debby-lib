a466fb79035f829c98e040b12f2e29466732c3dd
Semenov Fedor <fedor.v.semenov@gmail.com>
2022-07-10 20:18:20 +0500

    Fixed build scripts.

--- library.cmake
+++ library.cmake
@@ -7,7 +7,7 @@
 #      2021.11.24 Initial version.
 ################################################################################
 cmake_minimum_required (VERSION 3.11)
-project(debby CXX C)
+project(debby LANGUAGES CXX C)
 
 option(DEBBY__ENABLE_EXCEPTIONS "Enable exceptions for library" ON)
 option(DEBBY__ENABLE_SQLITE3 "Enable `Sqlite3` library" ON)
@@ -19,6 +19,20 @@ endif()
 
 if (DEBBY__ENABLE_EXCEPTIONS)
     set(PFS__ENABLE_EXCEPTIONS ON CACHE INTERNAL "")
+endif()
+
+portable_target(LIBRARY ${PROJECT_NAME} ALIAS pfs::debby)
+
+if (DEBBY__ENABLE_SQLITE3)
+    portable_target(SOURCES ${PROJECT_NAME}
+        ${CMAKE_CURRENT_LIST_DIR}/src/error.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/sqlite3.c
+        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/database.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/result.cpp
+        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/statement.cpp)
+
+    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "-DDEBBY__SQLITE3_ENABLED=1")
+    message(STATUS "`sqlite3` enabled")
 endif()
 
 if (NOT TARGET pfs::common)
@@ -37,26 +51,7 @@ if (DEBBY__ENABLE_ROCKSDB)
 
     portable_target(INCLUDE_PROJECT
         ${PORTABLE_TARGET__CURRENT_PROJECT_DIR}/cmake/RocksDB.cmake)
-endif()
 
-portable_target(LIBRARY ${PROJECT_NAME} ALIAS pfs::debby)
-portable_target(INCLUDE_DIRS ${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
-portable_target(LINK ${PROJECT_NAME} PUBLIC pfs::common)
-portable_target(EXPORTS ${PROJECT_NAME} DEBBY__EXPORTS DEBBY__STATIC)
-
-if (DEBBY__ENABLE_SQLITE3)
-    portable_target(SOURCES ${PROJECT_NAME}
-        ${CMAKE_CURRENT_LIST_DIR}/src/error.cpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/sqlite3.c
-        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/database.cpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/result.cpp
-        ${CMAKE_CURRENT_LIST_DIR}/src/sqlite3/statement.cpp)
-
-    portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "-DDEBBY__SQLITE3_ENABLED=1")
-    message(STATUS "`sqlite3` enabled")
-endif()
-
-if (DEBBY__ENABLE_ROCKSDB)
     portable_target(SOURCES ${PROJECT_NAME}
         ${CMAKE_CURRENT_LIST_DIR}/src/rocksdb/database.cpp)
     portable_target(DEFINITIONS ${PROJECT_NAME} PUBLIC "-DDEBBY__ROCKSDB_ENABLED=1")
@@ -64,3 +59,8 @@ if (DEBBY__ENABLE_ROCKSDB)
 
     message(STATUS "`RocksDB` enabled")
 endif()
+
+portable_target(INCLUDE_DIRS ${PROJECT_NAME} PUBLIC ${CMAKE_CURRENT_LIST_DIR}/include)
+portable_target(LINK ${PROJECT_NAME} PUBLIC pfs::common)
+portable_target(EXPORTS ${PROJECT_NAME} DEBBY__EXPORTS DEBBY__STATIC)
+
